384fa282719bbdc8a3b7d1deacf3039a4539c6b2
medoubia <mohamedoubia30@gmail.com>
2024-11-10 19:17:35 +0100

    chnage jenkinsFile

--- jest.config.js
+++ jest.config.js
@@ -1,16 +1,20 @@
 module.exports = {
-    preset: 'jest-preset-angular',
-    setupFilesAfterEnv: ['./src/setup-jest.ts'],
-    globalSetup: 'jest-preset-angular/global-setup',
-    testMatch: ['**/+(*.)+(spec).+(ts)'],
-    moduleFileExtensions: ['ts', 'html', 'js', 'json'],
-    collectCoverage: true,
-    coverageReporters: ['html', 'text-summary'],
-    coverageDirectory: 'coverage/angular-sample-small-project',
-    testEnvironment: 'jsdom',
-    globals: {
-      'ts-jest': {
-        tsconfig: '<rootDir>/tsconfig.spec.json'
-      }
+  preset: 'jest-preset-angular',
+  setupFilesAfterEnv: ['./src/setup-jest.ts'],
+  globalSetup: 'jest-preset-angular/global-setup',
+  testMatch: ['**/+(*.)+(spec).+(ts)'],
+  moduleFileExtensions: ['ts', 'html', 'js', 'json'],
+  collectCoverage: true,
+  coverageReporters: ['html', 'text-summary'],
+  coverageDirectory: 'coverage/angular-sample-small-project',
+  testEnvironment: 'jsdom',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json'
     }
-  };
+  },
+  reporters: [
+    'default',
+    ['jest-junit', { output: './junit.xml' }]  // Added reporter configuration for jest-junit
+  ]
+};
